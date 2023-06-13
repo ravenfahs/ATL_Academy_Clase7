8e19ec7c8f25197d15b98c0af37b2afb84bcd8f3
ravenfahs <raven_fahs@hotmail.com>
2023-06-12 22:24:15 -0500

    Ejercicios 1, 2, 3 - Determina cual número es menor y mayor en el array y el promedio

--- src/Ejercicio_1.java
+++ src/Ejercicio_1.java
@@ -1,6 +1,7 @@
 /*
 * 1. Hacer un programa que permita al usuario ingresar 5 números y mostrarlos.
 * 2. Después de realizar el ejercicio anterior, agregar que muestre el número mayor y el número menor.
+* 3. Ahora, hay que mostrar el promedio. Para hacer esto se suman todos los números y se divide por 5, ya que el usuario agregó 5 números
 */
 import java.util.Arrays;
 import java.util.Scanner;
@@ -11,8 +12,10 @@ public class Ejercicio_1 {
 
         //variables, se ajusta el tamaño del array a 5, se declaran las variables para determinar cuál es número son menor y mayor dentro del array
         int[] numeros = new int[5];
-        int numeroMenor = 0;
-        int numeroMayor = 0;
+        int numeroMenor = numeros[0];
+        int numeroMayor = numeros[0];
+        int contador = 0;
+        double promedio = 0;
 
         //Se hace captura y se guardan los números al usuario en for
         System.out.println("Ingresar 5 números enteros...");
@@ -22,23 +25,25 @@ public class Ejercicio_1 {
             System.out.println("Ingresa número:");
             Scanner capturaNumeros = new Scanner(System.in);
             int numero = capturaNumeros.nextInt();
+            //Antes de que llene al array se van sumando los números capturados
+            contador = contador + numero;
             numeros[i] = numero;
 
-            //Se asigna el valor en la posición cero del array a las variables nemeroMayor y numeroMenor
-            numeroMenor = numeros[0];
-            numeroMayor = numeros[0];
-
-            //Se comparan uno a uno los valores para determinar si el menor o mayor y asignar el valor a cada variable según corresponda 
-            if (numeros[i]<= numeroMenor){
+            //Se comparan uno a uno los valores para determinar si el menor o mayor y asignar el valor a cada variable según corresponda
+            if (numeros[i] < numeroMenor){
                 numeroMenor = numeros[i];
             }
-            if (numeros[i] >=numeroMayor) {
+            if (numeros[i] > numeroMayor) {
                 numeroMayor = numeros[i];
             }
         }
-        
+
+        //Promedio del array
+        promedio = (float) contador / numeros.length;
+
         //Mostrar el array como cadena con los números y cuál es el número menor y mayor dentro del array registrado
         System.out.println("Números registrados son: "+ Arrays.toString(numeros));
         System.out.println("Es el menor: " + numeroMenor + " Es el mayor: " + numeroMayor );
+        System.out.println("Suma de todos los números: "+ contador + " el promedio es: "+ promedio);
     }
 }
