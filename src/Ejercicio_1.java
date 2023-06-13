28502030311f8e00b1c2fa12eaf9d523c63bb128
ravenfahs <raven_fahs@hotmail.com>
2023-06-12 21:41:58 -0500

    Ejercicios 1 y 2 - Determina cual número es menor y mayor en el array

--- src/Ejercicio_1.java
+++ src/Ejercicio_1.java
@@ -1,5 +1,6 @@
 /*
-* Hacer un programa que permita al usuario ingresar 5 números y mostrarlos.
+* 1. Hacer un programa que permita al usuario ingresar 5 números y mostrarlos.
+* 2. Después de realizar el ejercicio anterior, agregar que muestre el número mayor y el número menor.
 */
 import java.util.Arrays;
 import java.util.Scanner;
@@ -8,8 +9,10 @@ public class Ejercicio_1 {
 
     public static void main(String[] args) {
 
-        //variables, se ajusta el tamaño del array a 5
+        //variables, se ajusta el tamaño del array a 5, se declaran las variables para determinar cuál es número son menor y mayor dentro del array
         int[] numeros = new int[5];
+        int numeroMenor = 0;
+        int numeroMayor = 0;
 
         //Se hace captura y se guardan los números al usuario en for
         System.out.println("Ingresar 5 números enteros...");
@@ -20,9 +23,22 @@ public class Ejercicio_1 {
             Scanner capturaNumeros = new Scanner(System.in);
             int numero = capturaNumeros.nextInt();
             numeros[i] = numero;
-        }
 
-        //Método para mostrar el array números-
-        System.out.println("Números registrados: "+ Arrays.toString(numeros));
+            //Se asigna el valor en la posición cero del array a las variables nemeroMayor y numeroMenor
+            numeroMenor = numeros[0];
+            numeroMayor = numeros[0];
+
+            //Se comparan uno a uno los valores para determinar si el menor o mayor y asignar el valor a cada variable según corresponda 
+            if (numeros[i]<= numeroMenor){
+                numeroMenor = numeros[i];
+            }
+            if (numeros[i] >=numeroMayor) {
+                numeroMayor = numeros[i];
+            }
+        }
+        
+        //Mostrar el array como cadena con los números y cuál es el número menor y mayor dentro del array registrado
+        System.out.println("Números registrados son: "+ Arrays.toString(numeros));
+        System.out.println("Es el menor: " + numeroMenor + " Es el mayor: " + numeroMayor );
     }
 }
