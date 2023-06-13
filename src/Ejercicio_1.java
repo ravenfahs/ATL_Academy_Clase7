b6bf9f39fd18b687c080de5d8978efc810c0a94f
ravenfahs <raven_fahs@hotmail.com>
2023-06-12 19:00:56 -0500

    Ejercicios 1 - Arrays

---
+++ src/Ejercicio_1.java
@@ -0,0 +1,28 @@
+/*
+* Hacer un programa que permita al usuario ingresar 5 números y mostrarlos.
+*/
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+
+        //variables, se ajusta el tamaño del array a 5
+        int[] numeros = new int[5];
+
+        //Se hace captura y se guardan los números al usuario en for
+        System.out.println("Ingresar 5 números enteros...");
+
+        //Ciclo de lectura y escritura en el array
+        for (int i = 0; i <numeros.length ; i++) {
+            System.out.println("Ingresa número:");
+            Scanner capturaNumeros = new Scanner(System.in);
+            int numero = capturaNumeros.nextInt();
+            numeros[i] = numero;
+        }
+
+        //Método para mostrar el array números-
+        System.out.println("Números registrados: "+ Arrays.toString(numeros));
+    }
+}
